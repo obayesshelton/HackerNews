5f9035d4234d178cde649f4d575bb83b977fb6f3
Oliver Bayes-shelton <oliver@Olivers-MacBook-Pro-2.local>
2013-09-04 19:29:08 +0100

    Test file

--- src/console.php
+++ src/console.php
@@ -1,4 +1,12 @@
+#!/usr/bin/env php
 <?php
+
+require_once __DIR__ . '/silex.phar';
+
+$loader->register();
+
+$app = new Silex\Application();
+$app['autoloader']->registerNamespace('Symfony', __DIR__ . '/../vendor');
 
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\InputInterface;
@@ -6,17 +14,27 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
-$console = new Application('My Silex Application', 'n/a');
-$console->getDefinition()->addOption(new InputOption('--env', '-e', InputOption::VALUE_REQUIRED, 'The Environment name.', 'dev'));
-$console
-    ->register('my-command')
-    ->setDefinition(array(
-        // new InputOption('some-option', null, InputOption::VALUE_NONE, 'Some help'),
-    ))
-    ->setDescription('My command description')
-    ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
-        // do something
-    })
-;
+$console = new Application('HackerNews', 'n/a');
+$console->register( 'sync' )
+  ->setDefinition( array(
+     //Create a "--test" optional parameter
+     new InputOption('test', '', InputOption::VALUE_NONE, 'Test mode'),
+    ) )
+  ->setDescription('Synchronize with an external data source')
+  ->setHelp('Usage: <info>./console.php sync [--test]</info>')
+  ->setCode(
+    function(InputInterface $input, OutputInterface $output) use ($app)
+    {
+      if ($input->getOption('test'))
+      {
+        $output->write("\n\tTest Mode Enabled\n\n");
+      }
 
-return $console;
+      $output->write( "Contacting external data source ...\n");
+      //Do work here
+      //Example:
+      //  $app[ 'myExtension' ]->doStuff();
+    }
+  );
+
+$console->run();
